b4719d7b55d3a795f36a04ff28a755b6f5a810d7
CamilaG-code <cguadagni.sistemas@gmail.com>
2025-05-19 19:20:21 -0300

    se pedia un long, no un string y tiraba error

--- src/main/java/org/buffer/agendaterapeutas/model/Session.java
+++ src/main/java/org/buffer/agendaterapeutas/model/Session.java
@@ -29,7 +29,7 @@ public class Session {
     public Session() {}
 
     // Getters y Setters
-    public String getIdSession() {
+    public Long getIdSession() {
         return String.valueOf(idSession);
     }
 
--- src/main/java/org/buffer/agendaterapeutas/repository/SessionRepository.java
+++ src/main/java/org/buffer/agendaterapeutas/repository/SessionRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long>{
-    boolean existsByIdSession(String sessionId);
+    boolean existsByIdSession(Long idSession);
 }
